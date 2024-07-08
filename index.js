b068ea7a6e18a7220c66cb374b8f30ca1bad20e7
zahrafeisal <zahrafeisal3@gmail.com>
2024-07-08 16:54:41 +0300

    used var to declare and assign variables

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution in this file!
+var customerName = 'bob'
+console.log(customerName)
+
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+}
+// upperCaseCustomerName();
+customerName = 'bob'
+
+
+var bestCustomer;
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+    return bestCustomer;
+}
+// setBestCustomer();
+bestCustomer;
+
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+    return bestCustomer;
+}
+// overwriteBestCustomer();
+bestCustomer;
+
+
+const leastFavoriteCustomer = 'tim'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'matthew';
+    return leastFavoriteCustomer;
+}
+console.log(changeLeastFavoriteCustomer());
